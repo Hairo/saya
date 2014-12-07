ab9674ec288cd29604ea12aadb167bdeff4337d4
Hairo <hairocr8@gmail.com>
2014-12-07 15:16:27 -0400

    refactored for future MAL support

--- plex-saya.py
+++ plex-saya.py
@@ -1,47 +1,44 @@
 #!/usr/bin/python3
 
-import xml.dom.minidom
+import xml.dom.minidom, configparser
 import urllib.request as ur
 import hummingbird as hb
 
-hum = hb.Hummingbird('User', 'Pass')
-bird = hum.get_library("User", status="currently-watching")
+cf = configparser.ConfigParser()
+cf.read('saya.conf')
 
 url = "http://localhost:32400/library/sections/1/recentlyViewed"
 doc = xml.dom.minidom.parse(ur.urlopen(url))
 attr = doc.getElementsByTagName("Video")[0].getAttribute("title")
 
-def get_anime_info(name):
-	keyword = max(name.split(" "), key=len).lower()
+username = cf["hummingbird.me"]["user"]
+passw = cf["hummingbird.me"]["password"]
+hum = hb.Hummingbird(username, passw)
+bird = hum.get_library(username, status="currently-watching")
 
-	for t in range(len(get_current_lib())):
-		res = get_current_lib().index("".join([x for x in get_current_lib() if keyword in x]))
-		aid = bird[res].anime.anime_id
-		ep_watched = bird[res].episodes_watched
-		break
-	
-	return [aid, ep_watched]
-
-def get_current_lib():
+def update_hb_lib():
 	titles = []
 	for i in range(len(bird)):
   		titles.append(bird[i].anime.title.lower())
 
-	return titles
-
-def update_lib():
 	ep_no = int(attr.split(" - ")[1])
 	ep_title = attr.split(" - ")[0]
+	
+	keyword = max(ep_title.split(" "), key=len).lower()
+
+	for t in range(len(titles)):
+		res = titles.index("".join([x for x in titles if keyword in x]))
+		hb_id = bird[res].anime.anime_id
+		ep_watched = bird[res].episodes_watched
+		break
+	
 	print(ep_title, ep_no)
+	print(hb_id, ep_watched)
 
-	hb_id = get_anime_info(ep_title)[0]
-	hb_ep = get_anime_info(ep_title)[1]
-	print(hb_id, hb_ep)
-
-	if ep_no <= hb_ep:
+	if ep_no <= ep_watched:
 		print("Ya lo viste...")
 	else:
 		print("No lo has visto!!")
 		hum.update_entry(hb_id, episodes_watched=ep_no)
 
-update_lib()
+update_hb_lib()
