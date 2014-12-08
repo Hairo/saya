1dc2d167cee3288c37485c6751cea1b6df042833
Hairo <hairocr8@gmail.com>
2014-12-07 22:05:07 -0400

    comments

--- plex-saya.py
+++ plex-saya.py
@@ -4,40 +4,45 @@ import xml.dom.minidom, configparser, time
 import urllib.request as ur
 import hummingbird as hb
 
+# read config file
 cf = configparser.ConfigParser()
-cf.read('saya.conf')
+cf.read('sayat.conf')
 
+# Get last watched item from plex xml data
 host = cf["plex"]["host"]
 port = cf["plex"]["port"]
-
 url = "http://"+host+":"+port+"/library/sections/1/recentlyViewed"
 doc = xml.dom.minidom.parse(ur.urlopen(url))
 attr = doc.getElementsByTagName("Video")[0].getAttribute("title")
 
 def update_hb_lib():
+	# hummingbird init
 	username = cf["hummingbird.me"]["user"]
 	passw = cf["hummingbird.me"]["password"]
 	hum = hb.Hummingbird(username, passw)
-	bird = hum.get_library(username, status="currently-watching")
 
+	# get currently watching list
+	bird = hum.get_library(username, status="currently-watching")
 	titles = []
 	for i in range(len(bird)):
   		titles.append(bird[i].anime.title.lower())
 
+	# split plex data to get the show name and episode watched
 	plex_ep = int(attr.split(" - ")[1])
 	ep_title = attr.split(" - ")[0]
-	
-	keyword = max(ep_title.split(" "), key=len).lower()
 
+	# get currently watching list data from hummingbird
+	keyword = max(ep_title.split(" "), key=len).lower()
 	for t in range(len(titles)):
 		res = titles.index("".join([x for x in titles if keyword in x]))
-		hb_id = bird[res].anime.anime_id
-		ep_watched = bird[res].episodes_watched
+		hb_id = bird[res].anime.anime_id 				# anime id
+		ep_watched = bird[res].episodes_watched			# watched count
 		break
 	
 	print(ep_title, plex_ep)
 	print(hb_id, ep_watched)
 
+	# check if already watched that episode
 	if ep_watched < plex_ep:
 		hum.update_entry(hb_id, episodes_watched=plex_ep)
 		print("List updated.")
